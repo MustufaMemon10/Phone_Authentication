e6d8f07227e252699de4609a3c3f6afcb97a2804
MustufaMemon10 <mustufasam06@gmail.com>
2024-06-29 10:17:07 +0530

    last commit

--- lib/screens/Verify_Phone.dart
+++ lib/screens/Verify_Phone.dart
@@ -29,101 +29,103 @@ class VerifyScreen extends StatelessWidget {
         color: AppColors.fieldColor,
       ),
     );
-    if (otpController.isLoading.value) {
-      return const CircularProgressIndicator(
-        color: AppColors.primary,
-        backgroundColor: Colors.white24,
-      );
-    }
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: const SAppbar(
-          leadingIcon: Icons.arrow_back,
-          showLeadingIcon: true,
-        ),
-        body: Padding(
-          padding:
-              const EdgeInsets.only(left: 11, right: 11, top: AppSizes.maxSize),
-          child: Center(
-            child: Column(children: [
-              const Text(
-                'Verify Phone',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: AppSizes.sm),
-              Text(
-                'Code is sent to $phoneNumber',
-                style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: AppColors.lightGrey),
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              Pinput(
-                controller: otpController.msgOtp,
-                length: 6,
-                defaultPinTheme: defaultPinTheme,
-                closeKeyboardWhenCompleted: true,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'OtpField is empty';
-                  }
-                  return null;
-                },
-                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
-              ),
-              const SizedBox(
-                height: AppSizes.defaultPadding,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Didn\'t receive the code?',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        letterSpacing: 0.7,
-                        color: AppColors.lightGrey),
-                  ),
-                  TextButton(
-                      onPressed: () => controller.loginWithPhoneNumber(),
-                      child: const Text(
-                        'Request Again',
-                        style: TextStyle(
-                            letterSpacing: 0.7,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: AppColors.primary),
-                      ))
-                ],
-              ),
-              const SizedBox(height: AppSizes.lg),
-              SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: AppSizes.btnPadding),
-                    ),
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      otpController.otpVerify();
-                    },
-                    child: Text('VERIFY AND CONTINUE',
-                        style: GoogleFonts.montserrat(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                        )),
-                  ))
-            ]),
-          ),
-        ));
+   return Obx(() {
+     if (otpController.isLoading.value) {
+       return const CircularProgressIndicator(
+         color: AppColors.primary,
+         backgroundColor: Colors.white24,
+       );
+     }
+     return Scaffold(
+         resizeToAvoidBottomInset: false,
+         appBar: const SAppbar(
+           leadingIcon: Icons.arrow_back,
+           showLeadingIcon: true,
+         ),
+         body: Padding(
+           padding:
+           const EdgeInsets.only(left: 11, right: 11, top: AppSizes.maxSize),
+           child: Center(
+             child: Column(children: [
+               const Text(
+                 'Verify Phone',
+                 style: TextStyle(
+                   fontSize: 22,
+                   fontWeight: FontWeight.bold,
+                 ),
+               ),
+               const SizedBox(height: AppSizes.sm),
+               Text(
+                 'Code is sent to $phoneNumber',
+                 style: const TextStyle(
+                     fontSize: 16,
+                     fontWeight: FontWeight.w400,
+                     color: AppColors.lightGrey),
+               ),
+               const SizedBox(
+                 height: 36,
+               ),
+               Pinput(
+                 controller: otpController.msgOtp,
+                 length: 6,
+                 defaultPinTheme: defaultPinTheme,
+                 closeKeyboardWhenCompleted: true,
+                 validator: (value) {
+                   if (value!.isEmpty) {
+                     return 'OtpField is empty';
+                   }
+                   return null;
+                 },
+                 pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+               ),
+               const SizedBox(
+                 height: AppSizes.defaultPadding,
+               ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   const Text(
+                     'Didn\'t receive the code?',
+                     style: TextStyle(
+                         fontSize: 16,
+                         fontWeight: FontWeight.w400,
+                         letterSpacing: 0.7,
+                         color: AppColors.lightGrey),
+                   ),
+                   TextButton(
+                       onPressed: () => controller.loginWithPhoneNumber(),
+                       child: const Text(
+                         'Request Again',
+                         style: TextStyle(
+                             letterSpacing: 0.7,
+                             fontSize: 16,
+                             fontWeight: FontWeight.w400,
+                             color: AppColors.primary),
+                       ))
+                 ],
+               ),
+               const SizedBox(height: AppSizes.lg),
+               SizedBox(
+                   width: double.infinity,
+                   child: ElevatedButton(
+                     style: ElevatedButton.styleFrom(
+                       padding: const EdgeInsets.symmetric(
+                           vertical: AppSizes.btnPadding),
+                     ),
+                     onPressed: () {
+                       FocusScope.of(context).unfocus();
+                       otpController.otpVerify();
+                     },
+                     child: Text('VERIFY AND CONTINUE',
+                         style: GoogleFonts.montserrat(
+                           fontSize: 16,
+                           fontWeight: FontWeight.w700,
+                           color: Colors.white,
+                         )),
+                   ))
+             ]),
+           ),
+         ));
+   });
   }
 }
--- lib/screens/phone_number_screen.dart
+++ lib/screens/phone_number_screen.dart
@@ -20,137 +20,139 @@ class _PhoneScreenState extends State<PhoneScreen> {
 
   @override
   Widget build(BuildContext context) {
-    if(controller.isLoading.value){
-      return const CircularProgressIndicator(
-        color: AppColors.primary,
-        backgroundColor: Colors.white24,
-      );
-    }
-    return
-      Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: const SAppbar(
-        showLeadingIcon: true,
-        leadingIcon: Icons.close,
-      ),
-      body:
-      Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-                left: AppSizes.defaultPadding,
-                right: AppSizes.defaultPadding,
-                top: AppSizes.maxSize),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Please enter your mobile number',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: AppSizes.sm),
-                const Text(
-                  'You\'ll receive a 6 digit code',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: AppColors.lightGrey),
-                ),
-                const Text(
-                  'to verify next.',
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: AppColors.lightGrey),
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0, vertical: 5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 1.0, color: Colors.black),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset(
-                        AppImage.india,
-                        height: 24,
-                        width: 24,
-                      ),
-                      const SizedBox(
-                        width: AppSizes.defaultPadding,
-                      ),
-                      const Text(
-                        '+91',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: AppSizes.defaultPadding,
-                      ),
-                      const Text(
-                        '-',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: AppSizes.defaultPadding,
-                      ),
-                      Expanded(
-                        child: TextFormField(
-                          controller: controller.phone,
-                          keyboardType: TextInputType.phone,
-                          decoration: const InputDecoration(
-                              hintText: 'Mobile Number',
-                              hintStyle: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                  color: AppColors.grey),
-                              border: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              focusedErrorBorder: InputBorder.none),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: AppSizes.lg,
-                ),
-                CustomButton(
-                  title: 'CONTINUE',
-                  onTap: () {
-                    FocusScope.of(context).unfocus();
-                    controller.loginWithPhoneNumber();
-                  },
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                AppImage.bg3,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+   return Obx((){
+     if(controller.isLoading.value){
+       return const CircularProgressIndicator(
+         color: AppColors.primary,
+         backgroundColor: Colors.white24,
+       );
+     }
+     return
+       Scaffold(
+         resizeToAvoidBottomInset: false,
+         appBar: const SAppbar(
+           showLeadingIcon: true,
+           leadingIcon: Icons.close,
+         ),
+         body:
+         Column(
+           children: [
+             Padding(
+               padding: const EdgeInsets.only(
+                   left: AppSizes.defaultPadding,
+                   right: AppSizes.defaultPadding,
+                   top: AppSizes.maxSize),
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   const Text(
+                     'Please enter your mobile number',
+                     style: TextStyle(
+                       fontSize: 22,
+                       fontWeight: FontWeight.bold,
+                     ),
+                   ),
+                   const SizedBox(height: AppSizes.sm),
+                   const Text(
+                     'You\'ll receive a 6 digit code',
+                     style: TextStyle(
+                         fontSize: 16,
+                         fontWeight: FontWeight.w400,
+                         color: AppColors.lightGrey),
+                   ),
+                   const Text(
+                     'to verify next.',
+                     style: TextStyle(
+                         fontSize: 14,
+                         fontWeight: FontWeight.w400,
+                         color: AppColors.lightGrey),
+                   ),
+                   const SizedBox(
+                     height: 32,
+                   ),
+                   Container(
+                     padding: const EdgeInsets.symmetric(
+                         horizontal: 10.0, vertical: 5),
+                     decoration: BoxDecoration(
+                       color: Colors.white,
+                       border: Border.all(width: 1.0, color: Colors.black),
+                     ),
+                     child: Row(
+                       crossAxisAlignment: CrossAxisAlignment.center,
+                       children: <Widget>[
+                         Image.asset(
+                           AppImage.india,
+                           height: 24,
+                           width: 24,
+                         ),
+                         const SizedBox(
+                           width: AppSizes.defaultPadding,
+                         ),
+                         const Text(
+                           '+91',
+                           style: TextStyle(
+                             fontSize: 16,
+                             fontWeight: FontWeight.w400,
+                           ),
+                         ),
+                         const SizedBox(
+                           width: AppSizes.defaultPadding,
+                         ),
+                         const Text(
+                           '-',
+                           style: TextStyle(
+                             fontSize: 16,
+                             fontWeight: FontWeight.w400,
+                           ),
+                         ),
+                         const SizedBox(
+                           width: AppSizes.defaultPadding,
+                         ),
+                         Expanded(
+                           child: TextFormField(
+                             controller: controller.phone,
+                             keyboardType: TextInputType.phone,
+                             decoration: const InputDecoration(
+                                 hintText: 'Mobile Number',
+                                 hintStyle: TextStyle(
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.w400,
+                                     color: AppColors.grey),
+                                 border: InputBorder.none,
+                                 enabledBorder: InputBorder.none,
+                                 focusedBorder: InputBorder.none,
+                                 errorBorder: InputBorder.none,
+                                 focusedErrorBorder: InputBorder.none),
+                           ),
+                         ),
+                       ],
+                     ),
+                   ),
+                   const SizedBox(
+                     height: AppSizes.lg,
+                   ),
+                   CustomButton(
+                     title: 'CONTINUE',
+                     onTap: () {
+                       FocusScope.of(context).unfocus();
+                       controller.loginWithPhoneNumber();
+                     },
+                   ),
+                 ],
+               ),
+             ),
+             Expanded(
+               child: Align(
+                 alignment: Alignment.bottomCenter,
+                 child: Image.asset(
+                   AppImage.bg3,
+                   fit: BoxFit.cover,
+                 ),
+               ),
+             ),
+           ],
+         ),
+       );
+   });
   }
 }
